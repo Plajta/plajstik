416f1fb297e489a6a9ddf8924981902fcabe32e3
Viktor Suchý <susenky.opavia@gmail.com>
2024-03-09 00:40:58 +0100

    dark mod a i ten white mod

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,13 +1,23 @@
-import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
+import { ThemeSwitcher } from "~/components/theme-switcher";
+import { ThemeProvider } from "~/components/theme-provider";
+import type { Metadata, Viewport } from "next";
 
 import { cn } from "~/lib/utils";
+
+import "./globals.css";
 
 const fontSans = FontSans({
     subsets: ["latin"],
     variable: "--font-sans",
 });
+
+export const viewport: Viewport = {
+    themeColor: [
+        { media: "(prefers-color-scheme: light)", color: "white" },
+        { media: "(prefers-color-scheme: dark)", color: "black" },
+    ],
+};
 
 export const metadata: Metadata = {
     title: "Plajstick",
@@ -23,7 +33,10 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
             <head />
             <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-                {children}
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                    <ThemeSwitcher />
+                    {children}
+                </ThemeProvider>
             </body>
         </html>
     );
