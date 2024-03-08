9293940f2ab035976aa01a4e27abe7c289d76636
Viktor Suchý <susenky.opavia@gmail.com>
2024-03-09 00:11:33 +0100

    shadcnui pridano do konfiguratoru

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
-    return <div className="text-3xl font-bold">Plajstick</div>;
+    return (
+        <div>
+            <Button>Click me</Button>
+        </div>
+    );
 }
