c1983f95d8af57131598fba67f881229b03b123d
Viktor Suchý <susenky.opavia@gmail.com>
2024-03-09 10:46:17 +0100

    odjebani dialogu

--- app/components/joystick-model.tsx
+++ app/components/joystick-model.tsx
@@ -1,14 +1,6 @@
 "use client";
 
 import { useGLTF, Html } from "@react-three/drei";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "~/components/ui/dialog";
 
 export function JoystickModel() {
     const { scene } = useGLTF("/models/joystick.glb");
@@ -19,20 +11,7 @@ export function JoystickModel() {
         if (o.userData.prop) {
             annotations.push(
                 <Html key={o.uuid} position={[o.position.x, o.position.y, o.position.z]} distanceFactor={0.25}>
-                    <div className="annotation">
-                        <Dialog>
-                            <DialogTrigger>{o.userData.prop}</DialogTrigger>
-                            <DialogContent className="z-[9999999999]">
-                                <DialogHeader>
-                                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                                    <DialogDescription>
-                                        This action cannot be undone. This will permanently delete your account and
-                                        remove your data from our servers.
-                                    </DialogDescription>
-                                </DialogHeader>
-                            </DialogContent>
-                        </Dialog>
-                    </div>
+                    <div className="annotation">{o.userData.prop}</div>
                 </Html>,
             );
         }
