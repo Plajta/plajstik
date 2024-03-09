76e9d3f7de9110dfd0513ffd89e677d796f23c76
Viktor Suchý <susenky.opavia@gmail.com>
2024-03-09 11:40:59 +0100

    hezcejsi styl na cary v labelu

--- app/components/button-label.tsx
+++ app/components/button-label.tsx
@@ -1,18 +1,25 @@
 "use client";
 
+import { useState } from "react";
 import { Object3D, Object3DEventMap } from "three";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
+import { Card, CardContent } from "~/components/ui/card";
 
 export function ButtonLabel({ object }: { object: Object3D<Object3DEventMap> }) {
+    const [bindingsOpen, setBindingsOpen] = useState<boolean>(false);
+
     return (
         <div className="annotation before:bg-slate-950">
             <Card className="shadow-none">
-                <CardContent className="select-none p-2">{object.userData.prop}</CardContent>
+                <CardContent className="select-none p-2" onClick={() => setBindingsOpen((prev) => !prev)}>
+                    {object.userData.prop}
+                </CardContent>
             </Card>
 
-            <Card className="shadow-none z-40 relative left-32 bottom-32 annotation-menu before:bg-slate-950">
-                <CardContent className="select-none p-2">aaaaa</CardContent>
-            </Card>
+            {bindingsOpen && (
+                <Card className="shadow-none z-40 absolute left-36 bottom-28 annotation-menu before:bg-slate-950">
+                    <CardContent className="select-none p-2">aaaaa</CardContent>
+                </Card>
+            )}
         </div>
     );
 }
