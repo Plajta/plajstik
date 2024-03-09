11ab8a0189a21d75508d75e99147ab62e74a5b5c
Viktor Suchý <susenky.opavia@gmail.com>
2024-03-09 12:00:39 +0100

    kompletni refactor systemu na otevrinai menucek

--- app/components/button-label.tsx
+++ app/components/button-label.tsx
@@ -1,21 +1,43 @@
 "use client";
 
-import { useState } from "react";
-import { Object3D, Object3DEventMap } from "three";
+import React, { useMemo, useState } from "react";
+import { Object3D } from "three";
 import { Card, CardContent } from "~/components/ui/card";
+import { LabelMenu } from "~/components/joystick-model";
 
-export function ButtonLabel({ object }: { object: Object3D<Object3DEventMap> }) {
-    const [bindingsOpen, setBindingsOpen] = useState<boolean>(false);
+interface ButtonLabelProps {
+    object: Object3D;
+    labelMenus: LabelMenu[];
+    setLabelMenus: React.Dispatch<React.SetStateAction<LabelMenu[]>>;
+}
+
+export function ButtonLabel({ object, labelMenus, setLabelMenus }: ButtonLabelProps) {
+    const menu = useMemo(() => labelMenus.find((item) => item.id === object.uuid), [labelMenus]);
 
     return (
         <div className="annotation before:bg-slate-950">
             <Card className="shadow-none">
-                <CardContent className="select-none p-2" onClick={() => setBindingsOpen((prev) => !prev)}>
+                <CardContent
+                    className="select-none p-2"
+                    onClick={() =>
+                        setLabelMenus((prev) =>
+                            prev.map((item) => {
+                                if (item.id === object.uuid) {
+                                    item.opened = !item.opened;
+                                } else {
+                                    item.opened = false;
+                                }
+
+                                return item;
+                            }),
+                        )
+                    }
+                >
                     {object.userData.prop}
                 </CardContent>
             </Card>
 
-            {bindingsOpen && (
+            {menu && menu.opened && (
                 <Card className="shadow-none z-40 absolute left-36 bottom-28 annotation-menu before:bg-slate-950">
                     <CardContent className="select-none p-2">aaaaa</CardContent>
                 </Card>
--- app/components/joystick-model.tsx
+++ app/components/joystick-model.tsx
@@ -2,21 +2,43 @@
 
 import { useGLTF, Html } from "@react-three/drei";
 import { ButtonLabel } from "~/components/button-label";
+import { useEffect, useState } from "react";
+import { Object3D } from "three";
+
+export interface LabelMenu {
+    id: string;
+    opened: boolean;
+}
 
 export function JoystickModel() {
     const { scene } = useGLTF("/models/joystick.glb");
 
-    const annotations: React.ReactNode[] = [];
+    const [labelMenus, setLabelMenus] = useState<LabelMenu[]>([]);
+    const [objects, setObjects] = useState<Object3D[]>([]);
 
-    scene.traverse((o) => {
-        if (o.userData.prop) {
-            annotations.push(
-                <Html key={o.uuid} position={[o.position.x, o.position.y, o.position.z]} distanceFactor={0.25}>
-                    <ButtonLabel object={o} />
-                </Html>,
-            );
-        }
-    });
+    useEffect(
+        () =>
+            scene.traverse((o) => {
+                if (o.userData.prop) {
+                    setLabelMenus((prev) => [...prev, { id: o.uuid, opened: false }]);
 
-    return <primitive object={scene}>{annotations}</primitive>;
+                    setObjects((prev) => [...prev, o]);
+                }
+            }),
+        [],
+    );
+
+    return (
+        <primitive object={scene}>
+            {objects.map((object) => (
+                <Html
+                    key={object.uuid}
+                    position={[object.position.x, object.position.y, object.position.z]}
+                    distanceFactor={0.25}
+                >
+                    <ButtonLabel object={object} labelMenus={labelMenus} setLabelMenus={setLabelMenus} />
+                </Html>
+            ))}
+        </primitive>
+    );
 }
