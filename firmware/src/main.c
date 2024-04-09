7a43a44f84302002ceff95221a2fe03a414f37e6
Vesek <venda.straka@gmail.com>
2024-04-09 07:50:40 +0200

    Cleanup and default config Default config that actually works? No way!

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -35,6 +35,7 @@
 #include "usb_descriptors.h"
 
 #include "../include/tiny-json.h"
+#include "../include/stringmap.h"
 
 #define TU_BIT(n)              (1UL << (n))
 
@@ -78,45 +79,6 @@ const int pins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int keymap[15] = {-1};
 int dpad[4] = {-1}; // Up, right, down, left
 
-int get_pin(char const* input) {
-  if (!strcmp(input, "btn0")) return 0; // These will get real names, that's why i used this
-  if (!strcmp(input, "btn1")) return 1;
-  if (!strcmp(input, "btn2")) return 2;
-  if (!strcmp(input, "btn3")) return 3;
-  if (!strcmp(input, "btn4")) return 4;
-  if (!strcmp(input, "btn5")) return 5;
-  if (!strcmp(input, "btn6")) return 6;
-  if (!strcmp(input, "btn7")) return 7;
-  if (!strcmp(input, "btn8")) return 8;
-  if (!strcmp(input, "btn9")) return 9;
-  return -1;
-};
-
-int find_mapping(char const* input) {
-  if (!strcmp(input, "a")) return 0; // WILL DO BETTER
-  if (!strcmp(input, "b")) return 1;
-  if (!strcmp(input, "x")) return 3;
-  if (!strcmp(input, "y")) return 4;
-  if (!strcmp(input, "l1")) return 6;
-  if (!strcmp(input, "r1")) return 7;
-  if (!strcmp(input, "l2")) return 8;
-  if (!strcmp(input, "r2")) return 9;
-  if (!strcmp(input, "select")) return 10;
-  if (!strcmp(input, "start")) return 11;
-  if (!strcmp(input, "home")) return 12;
-  if (!strcmp(input, "l3")) return 13;
-  if (!strcmp(input, "r3")) return 14;
-  return -1;
-};
-
-int find_dpad(char const* input) {
-  if (!strcmp(input, "dpad_u")) return 0;
-  if (!strcmp(input, "dpad_r")) return 1;
-  if (!strcmp(input, "dpad_d")) return 2;
-  if (!strcmp(input, "dpad_l")) return 3;
-  return -1;
-};
-
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
 //--------------------------------------------------------------------+
@@ -142,7 +104,7 @@ int main(void)
   adc_gpio_init(26);
   adc_gpio_init(27);
 
-  if (setup_from_json(buf) != 0) {
+  if (setup_from_json(default_json) != 0) {
     tud_cdc_write("Invalid JSON!", 13);
     tud_cdc_write_flush();
   }
